9b004ec5af875520f6f460600472e2712fb96ca2
Jack Liang <zily68074@gmail.com>
2020-04-26 21:44:49 +0800

    Updates

---
+++ precache-manifest.c31850cfeefd9b6a39e7060b1021ee48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78e7421f2b8b7d86ff46b8b58c7ec99",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "89dd9a00611044815485",
+    "url": "/react-todolist/static/css/main.d68b40da.chunk.css"
+  },
+  {
+    "revision": "09b6325aac927d79d7d7",
+    "url": "/react-todolist/static/js/2.94189343.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todolist/static/js/2.94189343.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89dd9a00611044815485",
+    "url": "/react-todolist/static/js/main.750b2e36.chunk.js"
+  },
+  {
+    "revision": "3d95843b87647e3070cd",
+    "url": "/react-todolist/static/js/runtime-main.eaf87c3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
